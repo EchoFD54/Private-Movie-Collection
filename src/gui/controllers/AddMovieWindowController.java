5c2b1cb434d45f563c4939bf972bcef07e6010fd
EchoFD54 <144889612+EchoFD54@users.noreply.github.com>
2024-01-08 09:37:57 +0100

    Update AddMovieWindowController.java

--- src/gui/controllers/AddMovieWindowController.java
+++ src/gui/controllers/AddMovieWindowController.java
@@ -1,6 +1,10 @@
 package gui.controllers;
 
 public class AddMovieWindowController {
+
+    private MainWindowController mainWindowController;
+
     public void setMainWindowController(MainWindowController mainWindowController) {
+        this.mainWindowController = mainWindowController;
     }
 }
