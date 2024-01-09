e70e40fa6c37d89a4764cd5c0eac0426cca59ecf
Nelson  Veríssimo <nelsonverissimo@Nelsons-MacBook-Pro.local>
2024-01-09 23:53:46 +0100

    Added bll managers
    connection between database and gui

--- src/bll/CategoryManager.java
+++ src/bll/CategoryManager.java
@@ -1,4 +1,64 @@
 package bll;
 
+import be.Category;
+import be.Movie;
+import dal.CategoryDAO;
+import dal.ICategoryDAO;
+
+import java.util.List;
+
 public class CategoryManager {
+    ICategoryDAO categoryDAO = new CategoryDAO();
+
+    /**
+     * Creates a Category on the Database
+     */
+    public void createCategory(Category category) {
+        categoryDAO.createCategory(category);
+    }
+
+    /**
+     * Updates a Category on the Database
+     */
+    public void updateCategory(String newCategoryName, String categoryName) {
+        categoryDAO.updateCategory(newCategoryName, categoryName);
+    }
+
+    /**
+     * Deletes a Category on the Database
+     */
+    public void deleteCategory(int categoryId) {
+        categoryDAO.deleteCategory(categoryId);
+    }
+
+    /**
+     * @return a list of categories
+     * Gets all the categories saved on the Database
+     */
+    public List<Category> getAllCategories() {
+        return categoryDAO.getAllCategories();
+    }
+
+    /**
+     * @return a list of movies
+     * Associates a movie to a category on the Database
+     */
+    public List<Movie> placeMovieOnCategory(int categoryId, int movieId){
+        return categoryDAO.placeMovieOnCategory(categoryId, movieId);
+    }
+
+    /**
+     * @return a list of movies
+     * Gets all the movies from a category saved on the Database
+     */
+    public List<Movie> getAllMoviesOfCategory(int categoryId){
+        return categoryDAO.getAllMoviesOfCategory(categoryId);
+    }
+
+    /**
+     * Deletes a movie from a category on the Database
+     */
+    public void deleteMovieOnPlaylist(int movieId, int categoryId){
+        categoryDAO.deleteMovieFromCategory(movieId, categoryId);
+    }
 }
--- src/bll/MovieManager.java
+++ src/bll/MovieManager.java
@@ -1,4 +1,39 @@
 package bll;
 
+import be.Movie;
+import dal.IMovieDAO;
+import dal.MovieDAO;
+
+import java.util.List;
+
 public class MovieManager {
+    IMovieDAO movieDAO = new MovieDAO();
+
+    /**
+     * Creates a Movie on the Database
+     */
+    public void createMovie(Movie m) {
+        movieDAO.createMovie(m);
+    }
+
+    /**
+     * Updates a Movie on the Database
+     */
+    public void updateMovie(Movie m){
+        movieDAO.updateMovie(m);
+    }
+
+    /**
+     * Deletes a Movie on the Database
+     */
+    public void deleteMovie(int movieId){
+        movieDAO.deleteMovie(movieId);
+    }
+
+    /**
+     * @return a list of all Movies saved on the Database
+     */
+    public List<Movie> getAllMovies() {
+        return movieDAO.getAllMovies();
+    }
 }
