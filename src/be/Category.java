a8440c29dca1f1b5dde66e5444e4a203a50d4055
Nelson  Veríssimo <nelsonverissimo@Nelsons-MacBook-Pro.local>
2024-01-09 13:20:24 +0100

    Added Category class

--- src/be/Category.java
+++ src/be/Category.java
@@ -1,4 +1,42 @@
 package be;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Category {
+
+    private StringProperty name;
+    private IntegerProperty id;
+    private StringProperty movies;
+
+    public Category(int id, String name) {
+        this.id = new SimpleIntegerProperty(id);
+        this.name = new SimpleStringProperty(name);
+    }
+
+    public Category(String name) {
+        this.name = new SimpleStringProperty(name);
+    }
+
+    public StringProperty getName() {
+        return name;
+    }
+
+    public IntegerProperty getId() {
+        return id;
+    }
+
+    public StringProperty getMovies() {
+        return movies;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public void setMovies(String movies) {
+        this.movies.set(movies);
+    }
 }
