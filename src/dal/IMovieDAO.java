b0a6ad97c72736c7168d0dcaa24cf66afe4a1df6
Nelson  Veríssimo <nelsonverissimo@Nelsons-MacBook-Pro.local>
2024-01-09 13:24:31 +0100

    Added IMovieDAO & MovieDAO
    Code to create, update and delete a movie;
    Get all movies.

--- src/dal/IMovieDAO.java
+++ src/dal/IMovieDAO.java
@@ -1,4 +1,28 @@
 package dal;
 
+import be.Movie;
+
+import java.util.List;
+
 public interface IMovieDAO {
+
+    /**
+     * Creates a Movie on the Database
+     */
+    void createMovie(Movie m);
+
+    /**
+     * Updates a Movie on the Database
+     */
+    void updateMovie(Movie m);
+
+    /**
+     * Deletes a Movie on the Database by the ID
+     */
+    void deleteMovie(int movieId);
+
+    /**
+     * @return a list of all Movies saved on the Database
+     */
+    List<Movie> getAllMovies();
 }
--- src/dal/MovieDAO.java
+++ src/dal/MovieDAO.java
@@ -1,4 +1,85 @@
 package dal;
 
-public class MovieDAO {
+import be.Movie;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieDAO implements IMovieDAO{
+    private final ConnectionManager cm = new ConnectionManager();
+
+    @Override
+    public void createMovie(Movie m) {
+        try(Connection con = cm.getConnection())
+        {
+            String sql = "INSERT INTO Movies(Name, IMDBRating, PersonalRating, FilePath, LastView) VALUES (?,?,?,?,?)";
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, m.getTitle().get());
+            pstmt.setString(2, m.getImdbRating().get());
+            pstmt.setString(3, m.getPersonalRating().get());
+            pstmt.setString(4, m.getFilePath().get());
+            pstmt.setString(5, m.getLastWatched().get());
+            pstmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Override
+    public void updateMovie(Movie m) {
+        try(Connection con = cm.getConnection())
+        {
+            String sql = "UPDATE Movies SET Name=?, PersonalRating=? WHERE Id=?";
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, m.getTitle().get());
+            pstmt.setString(2, m.getPersonalRating().get());
+            pstmt.setInt(3, m.getMovieId().get());
+            pstmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void deleteMovie(int movieId) {
+        try(Connection con = cm.getConnection())
+        {
+            String sql = "DELETE FROM CatMovie WHERE MoviesId=?";
+            String sql1 = "DELETE FROM Movies WHERE Id=?";
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            PreparedStatement pstmt1 = con.prepareStatement(sql1);
+            pstmt.setInt(1, movieId);
+            pstmt.execute();
+            pstmt1.setInt(1, movieId);
+            pstmt1.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Movie> getAllMovies() {
+        List<Movie> movies = new ArrayList<>();
+
+        try(Connection con = cm.getConnection())
+        {
+            String sql = "SELECT * FROM Movies";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                int id                = rs.getInt("Id");
+                String name           = rs.getString("Name");
+                String imdbRating     = rs.getString("IMDBRating");
+                String personalRating = rs.getString("PersonalRating");
+                String filePath       = rs.getString("FilePath");
+                String lastView       = rs.getString("LastView");
+
+                Movie m = new Movie(id, name, imdbRating, personalRating, filePath, lastView);
+                movies.add(m);
+            }
+            return movies;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
