c524b3f00e77910cd8fe353ee571587e468e3d92
Nacho P <144777550+coolandcalm@users.noreply.github.com>
2024-01-11 20:42:40 +0100

    Edit Category Button issue fixed

--- src/gui/controllers/EditCategoryController.java
+++ src/gui/controllers/EditCategoryController.java
@@ -7,22 +7,21 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class EditCategoryController {
-
+    @FXML
     protected TextField editCategoryName;
     private MainWindowController mainWindowController;
     private Category selectedCategory;
     private CategoryManager categoryManager = new CategoryManager();
 
-    @FXML
+
     public void setMainWindowController(MainWindowController controller){
         this.mainWindowController = controller;
     }
-    @FXML
+
     public void setSelectedCategoryName(Category categoryName){
         this.selectedCategory = categoryName;
         editCategoryName.setText(categoryName.getName().get());
     }
-    @FXML
     public void saveEditCategoryButton(){
         if (selectedCategory != null) {
             String newCategory = editCategoryName.getText().trim();
@@ -34,7 +33,6 @@ public class EditCategoryController {
             }
         }
     }
-    @FXML
     private void closeEditedWindow(){
         Stage stage = (Stage) editCategoryName.getScene().getWindow();
         stage.close();
