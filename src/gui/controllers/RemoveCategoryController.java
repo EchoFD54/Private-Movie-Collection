a88b888ebd8d727f17f6383f640afc1ab2f77369
Nacho P <144777550+coolandcalm@users.noreply.github.com>
2024-01-12 15:43:09 +0100

    Remove Category Update
    Cancel Remove Category Button function fixed and working

--- src/gui/controllers/RemoveCategoryController.java
+++ src/gui/controllers/RemoveCategoryController.java
@@ -1,15 +1,11 @@
 package gui.controllers;
 
-import be.Category;
-import bll.CategoryManager;
-import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
-import java.util.List;
-
 public class RemoveCategoryController {
+    @FXML
     public Button cancelRemoveCategoryBtn;
 
     public Button confirmRemoveCategoryBtn;
@@ -20,23 +16,16 @@ public class RemoveCategoryController {
         this.mainWindowController = controller;
     }
 
-    public void confirmRemoveCategory(ActionEvent actionEvent) {
+    public void confirmRemoveCategoryButton() {
         mainWindowController.removeCategory();
         Stage stage = (Stage) confirmRemoveCategoryBtn.getScene().getWindow();
         stage.close();
     }
-    public void cancelRemoveCategory(ActionEvent actionEvent) {
-        mainWindowController.removeCategory();
-        Stage stage = (Stage) confirmRemoveCategoryBtn.getScene().getWindow();
+    private void closeRemovedWindow(){
+        Stage stage = (Stage) cancelRemoveCategoryBtn.getScene().getWindow();
         stage.close();
     }
-
-    public void refreshCategoryList() {
-        ListView<Object> categoryList = null;
-        categoryList.getItems().clear();
-        CategoryManager categoryManager = new CategoryManager();
-        List<Category> allCategories = categoryManager.getAllCategories();
-        categoryList.getItems().addAll(allCategories);
+    public void cancelRemoveCategoryButton(){
+        closeRemovedWindow();
     }
-
 }
